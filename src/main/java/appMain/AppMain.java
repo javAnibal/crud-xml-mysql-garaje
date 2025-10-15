2de965a629c07a8bae0361ef0362e2e5cd0d91f8
anibaL <a88anibal@gmail.com>
2025-10-15 07:01:31 +0200

    Lectura y escritura XML completada

--- src/main/java/appMain/AppMain.java
+++ src/main/java/appMain/AppMain.java
@@ -3,37 +3,42 @@ package appMain;
 import controller.GarajeController;
 import model.Coche;
 import utils.GarajeException;
+import view.GarajeView;
 
 import java.nio.file.Path;
 
 public class AppMain {
 
 
-    private static final String RUTA_FICHERO =  Path.of(System.getProperty("user.dir"), "data", "garaje.xml").toString();
+    private static final String RUTA_FICHERO = Path.of(System.getProperty("user.dir"), "data", "garaje.xml").toString();
 
     public static void main(String[] args) {
 
 
         GarajeController garajeController = new GarajeController();
+        GarajeView garajeView = new GarajeView();
 
         //Creando Objetos
-        Coche c1 = new Coche(1,"8910JCY", "Nissan", "Pulsar");
-        Coche c2 = new Coche(2,"1234ABC", "Honda", "Civic");
+        Coche c1 = new Coche(1, "8910JCY", "Nissan", "Pulsar");
+        Coche c2 = new Coche(2, "1234ABC", "Honda", "Civic");
 
         //Agregando los coches
 
         garajeController.agregarCoche(c1);
         garajeController.agregarCoche(c2);
 
-        try{
+        try {
 
             garajeController.guardarGarajeEnXML(RUTA_FICHERO);
+            garajeView.mostrarGaraje(garajeController.getGaraje());
+
+            //  garajeController.cargarDesdeXML(RUTA_FICHERO);
+            //  garajeView.mostrarGaraje(garajeController.getGaraje());
 
 
         } catch (GarajeException e) {
             System.err.println("Error al guardar el fichero " + e.getMessage());
         }
-
 
 
     }
--- src/main/java/model/Garaje.java
+++ src/main/java/model/Garaje.java
@@ -14,8 +14,7 @@ public class Garaje {
 
     @XmlElementWrapper(name = "coches") // -> Elemento Padre
     @XmlElement(name = "coche") // -> Elemento hijo
-    private List<Coche> cochesRegistrados;
-
+    private List<Coche> cochesRegistrados = new ArrayList<>();
 
 
     public Garaje() {
@@ -28,13 +27,23 @@ public class Garaje {
     }
 
 
-
     //M() metodo propio
 
-    public void agregarCoche(Coche coche){
+    public void agregarCoche(Coche coche) {
 
         this.cochesRegistrados.add(coche);
     }
 
+    public boolean estaVacio() {
 
+        return this.cochesRegistrados.isEmpty();
+
+    }
+
+    @Override
+    public String toString() {
+        return "Garaje{" +
+                "cochesRegistrados=" + cochesRegistrados +
+                '}';
+    }
 }
